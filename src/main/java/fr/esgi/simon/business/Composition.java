a01c40fb875afd18db729582c208da6b7ac65d54
Driikzz <remisalles.12@gmail.com>
2024-10-23 15:21:30 +0200

    [ADD] : class

--- src/main/java/fr/esgi/simon/business/Composition.java
+++ src/main/java/fr/esgi/simon/business/Composition.java
@@ -4,12 +4,13 @@ import lombok.Data;
 import lombok.Getter;
 
 import java.util.List;
+import java.util.Map;
 
 @Data
 @Getter
 public class Composition {
     private Long id;
-    private List<Son> son;
+    private Map<List<Pad>, Double> sonComposition;
     private Joueur joueur;
     private static Long compteur = 0L;
 }
